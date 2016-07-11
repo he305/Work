ff5d6540ad8c17b5708cfc94304cff3eec226092
Sergey-notebook <he305@mail.ru>
2016-07-11 16:41:50 +0300

    11.07

---
+++ MNKConsoleTest/addcalculationsthread.cpp
@@ -0,0 +1,6 @@
+#include "addcalculationsthread.h"
+
+AddCalculationsThread::AddCalculationsThread()
+{
+
+}
---
+++ MNKConsoleTest/addcalculationsthread.h
@@ -0,0 +1,11 @@
+#ifndef ADDCALCULATIONSTHREAD_H
+#define ADDCALCULATIONSTHREAD_H
+
+
+class AddCalculationsThread
+{
+public:
+    AddCalculationsThread();
+};
+
+#endif // ADDCALCULATIONSTHREAD_H
